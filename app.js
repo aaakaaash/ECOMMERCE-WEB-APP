a341fed310ca831e6792aea2ffd0d64aa1dbca81
Akash Ravikumar <akashravikumar2015@gmail.com>
2024-10-21 23:07:49 +0530

    new error fixes

--- app.js
+++ app.js
@@ -24,11 +24,23 @@ const methodOverride = require('method-override');
 
 db();
 
+const allowedOrigins = ['http://localhost:3000', 'https://akashravikumar.live'];
+
 app.use(cors({
-    origin: 'http://localhost:3000', 
+    origin: function(origin, callback) {
+
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
+
 
 app.use(methodOverride((req, res) => {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
